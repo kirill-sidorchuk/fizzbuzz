42093f41896a8961be76f2cc3a2fd8145813224e
Vladislav Kovchug <vladislav.kovchug@teamdev.com>
2016-08-06 17:37:47 +0300

    PaperFolder. Implement PaperFolder using Fraction.

--- src/com/teamdev/fold/FoldLine.java
+++ src/com/teamdev/fold/FoldLine.java
@@ -1,5 +1,6 @@
 package com.teamdev.fold;
 
+import com.teamdev.Fraction;
 import com.teamdev.Vertex;
 
 /**
@@ -8,18 +9,34 @@ import com.teamdev.Vertex;
 public class FoldLine {
     private double k;
     private double c;
+    private Fraction fractionK;
+    private Fraction fractionC;
 
-    public FoldLine(double k, double c) {
-        this.k = k;
-        this.c = c;
+    public FoldLine(Fraction k, Fraction c) {
+        this.fractionK = k;
+        this.fractionC = c;
+        this.k = k.getFloatValue();
+        this.c = c.getFloatValue();
     }
 
     public FoldLine(Vertex v1, Vertex v2) {
         final Vertex bottomPoint = v1.getFloatY() <= v2.getFloatY() ? v1 : v2;
         final Vertex topPoint = v1.getFloatY() > v2.getFloatY() ? v1 : v2;
 
-        this.k = (bottomPoint.getFloatY() - topPoint.getFloatY())/(topPoint.getFloatX() - bottomPoint.getFloatX())*-1;
-        this.c = (bottomPoint.getFloatX()*topPoint.getFloatY() - topPoint.getFloatX()*bottomPoint.getFloatY())*-1;
+        //k = -1 * (bottomPoint.y - topPoint.y) / ()
+        //c = -1 * ( bottomPoint.x * topPoint.y - topPoint.x * bottomPoint.y )
+        this.fractionK = new Fraction(-1, 1).mul(bottomPoint.y.sub(topPoint.y).div(topPoint.x.sub(bottomPoint.x)));
+        this.fractionC = new Fraction(-1, 1).mul(bottomPoint.x.mul(topPoint.y).sub(topPoint.x.mul(bottomPoint.y)));
+        this.k = this.fractionK.getFloatValue();
+        this.c = this.fractionC.getFloatValue();
+    }
+
+    public Fraction getFractionK() {
+        return fractionK;
+    }
+
+    public Fraction getFractionC() {
+        return fractionC;
     }
 
     public double getK() {
