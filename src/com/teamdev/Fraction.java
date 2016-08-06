4fe442f920854d84964fca5b73f76f48924b53bc
Kirill <kirill.sidorchuk@teamdev.com>
2016-08-06 21:00:05 +0300

    fixing normalization bug

--- src/com/teamdev/Fraction.java
+++ src/com/teamdev/Fraction.java
@@ -47,13 +47,19 @@ public class Fraction implements Comparable<Fraction> {
     public Fraction normalize() {
         if( n == 0 )
             d = 1;
-        else
-            for( int i=0; i<20; ++i) {
+        else {
+            for (int i = 0; i < 20; ++i) {
                 long x = common_divider(n, d);
                 if (x == 1) break;
                 n /= x;
                 d /= x;
             }
+
+            if (d < 0) {
+                d = -d;
+                n = -n;
+            }
+        }
         return this;
     }
 
